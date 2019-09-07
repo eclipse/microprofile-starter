002a9c4327ec271e803a343ae3a33ec3f7e3dc51
Emily Jiang <emijiang6@googlemail.com>
2019-09-07 16:57:05 +0100

    #211 fix the secondar_url
    Signed-off-by: Emily Jiang <emijiang6@googlemail.com>

--- src/main/java/org/eclipse/microprofile/starter/addon/microprofile/servers/MicroprofileServersAddon.java
+++ src/main/java/org/eclipse/microprofile/starter/addon/microprofile/servers/MicroprofileServersAddon.java
@@ -195,7 +195,11 @@ public class MicroprofileServersAddon extends AbstractMicroprofileAddon {
         variables.put("jar_file", defineJarFileName(supportedServer, artifactId));
         variables.put("jar_parameters", defineJarParameters(supportedServer));
         variables.put("test_url", defineTestURL(supportedServer, artifactId));
-        variables.put("secondary_url", defineSecondaryURL(supportedServer, artifactId));
+        if (supportedServer != SupportedServer.LIBERTY) {
+            variables.put("secondary_url", defineSecondaryURL(supportedServer, artifactId));
+        } else {
+            variables.put("secondary_url", defineSecondaryURL(supportedServer, ""));
+        }
         variables.put("artifact_id", artifactId);
 
         String rootJava = getJavaApplicationRootPackage(model);
