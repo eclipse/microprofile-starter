da8ddb2299dbee2dae22f879000bee841c98515a
Rudy De Busscher <rdebusscher@gmail.com>
2020-06-23 21:20:16 +0200

    Add link to release tag on github for each specification (the specific version depending on the MP version)

---
+++ src/main/java/org/eclipse/microprofile/starter/addon/microprofile/servers/model/VersionSpecMatrix.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclipse.microprofile.starter.addon.microprofile.servers.model;
+
+import org.eclipse.microprofile.starter.core.model.MicroProfileVersion;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public final class VersionSpecMatrix {
+
+    private static final VersionSpecMatrix MATRIX = new VersionSpecMatrix();
+
+    private Map<MicroProfileVersion, Map<MicroprofileSpec, String>> data;
+
+    private VersionSpecMatrix() {
+        init();
+    }
+
+    private void init() {
+        data = new EnumMap<>(MicroProfileVersion.class);
+
+        data.put(MicroProfileVersion.MP12,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.1")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "1.0")
+                .add(MicroprofileSpec.JWT_AUTH, "1.0")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "1.0")
+                .add(MicroprofileSpec.METRICS, "1.0"));
+
+        data.put(MicroProfileVersion.MP13,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.2")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "1.0")
+                .add(MicroprofileSpec.JWT_AUTH, "1.0")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "1.0")
+                .add(MicroprofileSpec.OPEN_API, "1.0")
+                .add(MicroprofileSpec.REST_CLIENT, "1.0")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.0")
+                .add(MicroprofileSpec.METRICS, "1.1"));
+
+        data.put(MicroProfileVersion.MP14,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.3")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "1.1")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "1.0")
+                .add(MicroprofileSpec.OPEN_API, "1.0")
+                .add(MicroprofileSpec.REST_CLIENT, "1.1")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.1")
+                .add(MicroprofileSpec.METRICS, "1.1"));
+
+        data.put(MicroProfileVersion.MP20,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.3")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "1.1")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "1.0")
+                .add(MicroprofileSpec.OPEN_API, "1.0")
+                .add(MicroprofileSpec.REST_CLIENT, "1.1")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.1")
+                .add(MicroprofileSpec.METRICS, "1.1"));
+
+        data.put(MicroProfileVersion.MP21,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.3")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "1.1")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "1.0")
+                .add(MicroprofileSpec.OPEN_API, "1.0")
+                .add(MicroprofileSpec.REST_CLIENT, "1.1")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.2")
+                .add(MicroprofileSpec.METRICS, "1.1"));
+
+        data.put(MicroProfileVersion.MP22,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.3")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "2.0")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "1.0")
+                .add(MicroprofileSpec.OPEN_API, "1.1")
+                .add(MicroprofileSpec.REST_CLIENT, "1.2")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.3")
+                .add(MicroprofileSpec.METRICS, "1.1"));
+
+        data.put(MicroProfileVersion.MP30,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.3")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "2.0")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "2.0")
+                .add(MicroprofileSpec.OPEN_API, "1.1")
+                .add(MicroprofileSpec.REST_CLIENT, "1.3")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.3")
+                .add(MicroprofileSpec.METRICS, "2.0"));
+
+        data.put(MicroProfileVersion.MP32,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.3")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "2.0")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "2.1")
+                .add(MicroprofileSpec.OPEN_API, "1.1")
+                .add(MicroprofileSpec.REST_CLIENT, "1.3")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.3")
+                .add(MicroprofileSpec.METRICS, "2.2"));
+
+        data.put(MicroProfileVersion.MP33,
+                new EnumMapBuilder()
+                .add(MicroprofileSpec.CONFIG, "1.4")
+                .add(MicroprofileSpec.FAULT_TOLERANCE, "2.1")
+                .add(MicroprofileSpec.JWT_AUTH, "1.1")
+                .add(MicroprofileSpec.HEALTH_CHECKS, "2.2")
+                .add(MicroprofileSpec.OPEN_API, "1.1")
+                .add(MicroprofileSpec.REST_CLIENT, "1.4")
+                .add(MicroprofileSpec.OPEN_TRACING, "1.3")
+                .add(MicroprofileSpec.METRICS, "2.3"));
+    }
+
+    public Map<MicroprofileSpec, String> getSpecData(MicroProfileVersion version) {
+        return data.get(version);
+    }
+
+    public static VersionSpecMatrix getInstance() {
+        return MATRIX;
+    }
+
+    private static class EnumMapBuilder extends EnumMap<MicroprofileSpec, String> {
+
+        public EnumMapBuilder() {
+            super(MicroprofileSpec.class);
+        }
+
+        public EnumMapBuilder add(MicroprofileSpec spec, String version) {
+            put(spec, version);
+            return this;
+        }
+    }
+}
---
+++ src/main/java/org/eclipse/microprofile/starter/view/renderer/CustomSelectManyCheckboxRenderer.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclipse.microprofile.starter.view.renderer;
+
+import org.eclipse.microprofile.starter.addon.microprofile.servers.model.MicroprofileSpec;
+import org.primefaces.component.selectmanycheckbox.SelectManyCheckbox;
+import org.primefaces.component.selectmanycheckbox.SelectManyCheckboxRenderer;
+import org.primefaces.component.tooltip.Tooltip;
+import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.WidgetBuilder;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.model.SelectItem;
+import java.io.IOException;
+
+public class CustomSelectManyCheckboxRenderer extends SelectManyCheckboxRenderer {
+
+    private UIComponent labelFacet;
+    private UIComponent tooltipFacet;
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        labelFacet = component.getFacet("label");
+        tooltipFacet = component.getFacet("tooltip");
+        super.encodeEnd(context, component);
+    }
+
+    protected void encodeOptionLabel(FacesContext context, SelectManyCheckbox checkbox, String containerClientId, SelectItem option,
+                                     boolean disabled) throws IOException {
+
+        if (labelFacet == null) {
+            super.encodeOptionLabel(context, checkbox, containerClientId, option, disabled);
+        } else {
+
+            ResponseWriter writer = context.getResponseWriter();
+
+            writer.startElement("label", null);
+            if (disabled) {
+                writer.writeAttribute("class", "ui-state-disabled", null);
+            }
+
+            writer.writeAttribute("for", containerClientId, null);
+            // Added ':label' here so that we have an 'anchor' for the tooltip
+            String target = containerClientId + UINamingContainer.getSeparatorChar(context) + "label";
+            writer.writeAttribute("id", target, null);
+
+            if (option.getDescription() != null) {
+                writer.writeAttribute("title", option.getDescription(), null);
+            }
+
+            context.getExternalContext().getRequestMap().put("item", MicroprofileSpec.valueFor(option.getValue().toString()));
+            encodeLabel(context, option, target);
+            writer.endElement("label");
+        }
+    }
+
+
+    protected void encodeLabel(FacesContext context, SelectItem option, String target) throws IOException {
+        labelFacet.encodeAll(context);  // From the facet
+        if (tooltipFacet != null) {
+            // Add the tooltip for this option.
+
+            Tooltip tooltip = new Tooltip();
+            tooltip.setFor(target);
+            tooltip.getChildren().add(tooltipFacet);
+
+            encodeMarkupTooltip(context, tooltip, target);
+            encodeScriptTooltip(context, tooltip, target);
+
+        }
+
+    }
+
+    protected void encodeMarkupTooltip(FacesContext context, Tooltip tooltip, String target) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        if (target != null) {
+            String styleClass = tooltip.getStyleClass();
+            styleClass = styleClass == null ? Tooltip.CONTAINER_CLASS : Tooltip.CONTAINER_CLASS + " " + styleClass;
+            styleClass = styleClass + " ui-tooltip-" + tooltip.getPosition();
+
+            writer.startElement("div", tooltip);
+            writer.writeAttribute("id", tooltip.getClientId(context), null);
+            writer.writeAttribute("class", styleClass, "styleClass");
+
+            if (tooltip.getStyle() != null) {
+                writer.writeAttribute("style", tooltip.getStyle(), "style");
+            }
+
+            writer.startElement("div", tooltip);
+            writer.writeAttribute("class", "ui-tooltip-arrow", null);
+            writer.endElement("div");
+
+            writer.startElement("div", tooltip);
+            writer.writeAttribute("class", "ui-tooltip-text ui-shadow ui-corner-all", null);
+
+            if (tooltip.getChildCount() > 0) {
+                renderChildren(context, tooltip);
+            } else {
+                String valueToRender = ComponentUtils.getValueToRender(context, tooltip);
+                if (valueToRender != null) {
+                    if (tooltip.isEscape()) {
+                        writer.writeText(valueToRender, "value");
+                    } else {
+                        writer.write(valueToRender);
+                    }
+                }
+            }
+
+            writer.endElement("div");
+
+            writer.endElement("div");
+        }
+    }
+
+    protected void encodeScriptTooltip(FacesContext context, Tooltip tooltip, String target) throws IOException {
+        String clientId = tooltip.getClientId(context);
+        WidgetBuilder wb = getWidgetBuilder(context);
+        wb.init("Tooltip", tooltip.resolveWidgetVar(), clientId)
+                .attr("showEvent", tooltip.getShowEvent(), null)
+                .attr("hideEvent", tooltip.getHideEvent(), null)
+                .attr("showEffect", tooltip.getShowEffect(), null)
+                .attr("hideEffect", tooltip.getHideEffect(), null)
+                .attr("showDelay", tooltip.getShowDelay(), 150)
+                .attr("hideDelay", tooltip.getHideDelay(), 0)
+                .attr("target", target, null)
+                .attr("globalSelector", tooltip.getGlobalSelector(), null)
+                .attr("escape", tooltip.isEscape(), true)
+                .attr("trackMouse", tooltip.isTrackMouse(), false)
+                .attr("position", tooltip.getPosition(), "right")
+                .attr("delegate", tooltip.isDelegate(), false)
+                .returnCallback("beforeShow", "function()", tooltip.getBeforeShow())
+                .callback("onShow", "function()", tooltip.getOnShow())
+                .callback("onHide", "function()", tooltip.getOnHide());
+
+        wb.finish();
+    }
+}
