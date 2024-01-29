0f7893b41e292491f1f812caa84594cec4160932
Majid Mostafavi <majid@majidmostafavi.com>
2024-01-29 21:14:39 +0330

    Add MicroProfile Version 6

--- src/main/java/org/eclipse/microprofile/starter/core/TemplateVariableProvider.java
+++ src/main/java/org/eclipse/microprofile/starter/core/TemplateVariableProvider.java
@@ -64,7 +64,7 @@ public class TemplateVariableProvider {
             result.put("se_version", seVersion.getCode());
         }
 
-        if (model.getSpecification().getMicroProfileVersion() == MicroProfileVersion.MP50) {
+        if (model.getSpecification().getMicroProfileVersion() == MicroProfileVersion.MP50||model.getSpecification().getMicroProfileVersion() == MicroProfileVersion.MP60) {
             result.put("jakarta_ee_package", "jakarta");
 
         } else {
